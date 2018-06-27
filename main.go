30a9be3303bcb040aa5e2f51ebc04798c753b04e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-06-27 22:35:22 +0900

    handle multiple device

--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"sync"
 
 	"gopkg.in/yaml.v2"
 
 	"github.com/gvalkov/golang-evdev"
+)
+
+var (
+	verbose bool
 )
 
 type Config struct {
@@ -16,11 +22,11 @@ type Config struct {
 	Press     string `yaml:"press"`
 	Release   string `yaml:"release"`
 	LongPress string `yaml:"longpress"`
-	verbose   bool
+	uinput    *evdev.InputDevice
 }
 
 func (c *Config) exec(n, s string) {
-	if c.verbose {
+	if verbose {
 		log.Print(n + " triggered")
 	}
 	if s == "" {
@@ -47,24 +53,48 @@ func (c *Config) longpress() {
 	c.exec("long press", c.LongPress)
 }
 
-func main() {
-	var cfg Config
+func (c *Config) watch(wg *sync.WaitGroup) {
+	defer wg.Done()
 
-	var device, config string
-	var verbose bool
-	flag.StringVar(&config, "c", "", "config file")
-	flag.BoolVar(&verbose, "v", false, "verbose")
-	flag.Parse()
-
-	if config != "" {
-		f, err := os.Open(config)
+	last := int64(0)
+	for {
+		ev, err := c.uinput.ReadOne()
 		if err != nil {
 			log.Fatal(err)
 		}
+		if ev.Type != evdev.EV_KEY {
+			continue
+		}
+		switch ev.Value {
+		case 1:
+			c.press()
+			last = ev.Time.Nano()
+		case 0:
+			if last != 0 && ev.Time.Nano()-last > 10000000 {
+				c.release()
+			} else {
+				c.longpress()
+			}
+			last = 0
+		}
+	}
+}
+
+func main() {
+	var cfg []*Config
+
+	var config string
+	flag.StringVar(&config, "c", filepath.Join(os.Getenv("HOME"), ".config", "uinputd"), "config file")
+	flag.BoolVar(&verbose, "v", false, "verbose")
+	flag.Parse()
+
+	f, err := os.Open(config)
+	if err != nil {
+		log.Print(err)
+	} else {
 		yaml.NewDecoder(f).Decode(&cfg)
 		f.Close()
 	}
-	cfg.verbose = verbose
 
 	devs, err := evdev.ListInputDevices()
 	if err != nil {
@@ -76,47 +106,30 @@ func main() {
 		}
 	}()
 
-	var input *evdev.InputDevice
-	if device != "" {
-		for _, dev := range devs {
-			if verbose {
-				log.Println(dev.Name)
-			}
-			if input == nil && dev.Name == device {
-				input = dev
+	var wg sync.WaitGroup
+	n := 0
+
+	for _, dev := range devs {
+		if verbose {
+			log.Println("found uinput device", dev.Name)
+		}
+		for _, c := range cfg {
+			if dev.Phys == c.Device {
+				c.uinput = dev
+				wg.Add(1)
+				n++
+				if verbose {
+					log.Println("connected", dev.Name)
+				}
+				go c.watch(&wg)
 			}
 		}
-	} else if len(devs) > 0 {
-		input = devs[0]
+
 	}
 
-	if input == nil {
+	if n == 0 {
 		log.Fatal("cannot open uinput device")
 	}
-	if verbose {
-		log.Println("connected", input.Name)
-	}
 
-	last := int64(0)
-	for {
-		ev, err := input.ReadOne()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if ev.Type != evdev.EV_KEY {
-			continue
-		}
-		switch ev.Value {
-		case 1:
-			cfg.press()
-			last = ev.Time.Nano()
-		case 0:
-			if last != 0 && ev.Time.Nano()-last > 10000000 {
-				cfg.release()
-			} else {
-				cfg.longpress()
-			}
-			last = 0
-		}
-	}
+	wg.Wait()
 }
